a68a26632ca0e64810e6034a626a11eb6109db69
GWCMelody <melody2011102@gmail.com>
2020-09-08 22:10:31 +0800

    initial commit

---
+++ src/main/java/Model/Author.java
@@ -0,0 +1,63 @@
+package Model;
+
+import java.util.Objects;
+
+public class Author {
+    private String name;
+    private int numberOfBooks;
+    private String nationality;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author)) return false;
+        Author author = (Author) o;
+        return getNumberOfBooks() == author.getNumberOfBooks() &&
+                Objects.equals(getName(), author.getName()) &&
+                Objects.equals(getNationality(), author.getNationality());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getNumberOfBooks(), getNationality());
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", numberOfBooks=" + numberOfBooks +
+                ", nationality='" + nationality + '\'' +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumberOfBooks(int numberOfBooks) {
+        this.numberOfBooks = numberOfBooks;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfBooks() {
+        return numberOfBooks;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public Author(String name, int numberOfBooks, String nationality) {
+        this.name = name;
+        this.numberOfBooks = numberOfBooks;
+        this.nationality = nationality;
+    }
+}
---
+++ src/main/java/Model/Book.java
@@ -0,0 +1,87 @@
+package Model;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String isbn;
+    private String publisher;
+    private int year;
+    private String author;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book author1 = (Book) o;
+        return getYear() == author1.getYear() &&
+                Objects.equals(getTitle(), author1.getTitle()) &&
+                Objects.equals(getIsbn(), author1.getIsbn()) &&
+                Objects.equals(getPublisher(), author1.getPublisher()) &&
+                Objects.equals(getAuthor(), author1.getAuthor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getIsbn(), getPublisher(), getYear(), getAuthor());
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Book(String title, String isbn, String publisher, int year, String author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.year = year;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", year=" + year +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
